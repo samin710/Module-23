b5bf179c98317fce099005696c05d0e88dee6847
samin710 <smsaminii112@gmail.com>
2025-02-16 21:38:33 +0600

    task4 done

---
+++ task4.js
@@ -0,0 +1,32 @@
+// You are given an array of phone objects, each containing information about the model, brand, and price. Your task is to write a JavaScript function named findAveragePhonePrice that takes this array as input and returns the average price of phone.
+
+// Input
+
+//     const phones = [
+//         { model: "PhoneA", brand: "Iphone", price: 95000 },
+//         { model: "PhoneB", brand: "Samsung", price: 40000 },
+//         { model: "PhoneC", brand: "Oppo", price: 26000 },
+//         { model: "PhoneD", brand: "Nokia", price: 35000 },
+//         { model: "PhoneE", brand: "Iphone", price: 105000 },
+//         { model: "PhoneF", brand: "HTC", price: 48000 },
+//     ];
+
+function findAveragePhonePrice(phones) {
+  let size = phones.length,
+    sum = 0;
+  for (let i of phones) {
+    sum += i.price;
+  }
+  return Math.round(sum / size);
+}
+
+const phones = [
+  { model: "PhoneA", brand: "iPhone", price: 95000 },
+  { model: "PhoneB", brand: "Samsung", price: 40000 },
+  { model: "PhoneC", brand: "Oppo", price: 26000 },
+  { model: "PhoneD", brand: "Nokia", price: 35000 },
+  { model: "PhoneE", brand: "Iphone", price: 105000 },
+  { model: "PhoneF", brand: "HTC", price: 48000 },
+];
+
+console.log(findAveragePhonePrice(phones));
